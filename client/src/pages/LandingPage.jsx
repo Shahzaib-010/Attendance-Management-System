a7ee751107703c46ef46f5b53e36ade0f65e5b46
Shahzaib Anwar <user.name like : "shahzaibanwar1919@gmail.com>
2025-12-11 12:31:43 +0500

    project structure with server

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function landingPage() {
+  return (
+    <section>
+        {/* Navbar */}
+
+            <nav className="w-full h-20 bg-black text-white">
+              <p>Hello</p>
+            </nav>
+
+
+        {/* Landing Page */}
+
+            <div className="">
+
+
+            </div>
+
+
+
+        {/* About */}
+
+            <div className="">
+
+                
+            </div>
+
+
+        {/* footer */}
+    
+            <div className="">
+
+                
+            </div>
+
+
+    </section>
+  )
+}
+
+export default landingPage
